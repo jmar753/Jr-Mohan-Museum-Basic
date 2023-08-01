caeccaadfa2a7e987f5a46ea08973970c05800c1
Jr Mohan <jmohan6@myseneca.ca>
2023-08-01 12:19:46 -0400

    step 7 done, still bug testing

--- components/MainNav.js
+++ components/MainNav.js
@@ -11,6 +11,7 @@ import Link from 'next/link';
 import { useAtom } from 'jotai';
 import { searchHistoryAtom } from '@/store';
 import { addToHistory } from '@/lib/userData';
+import { readToken, removeToken } from '@/lib/authenticate';
 
 export default function MainNav() {
 	const [searchField, setSearchField] = useState('');
@@ -18,12 +19,19 @@ export default function MainNav() {
 	const [searchHistory, setSearchHistory] = useAtom(searchHistoryAtom)
  
 	const router = useRouter();
+	let token = readToken();
 
 	async function submitForm(e) {
 		e.preventDefault();
 		setIsExpanded(false)
 		setSearchHistory(await addToHistory(`title=true&q=${searchField}`)) 
 		router.push(`/artwork?title=true&q=${searchField}`)
+	}
+
+	function logout() {
+		setIsExpanded(false)
+		removeToken();
+		router.push('/login');
 	}
 	
     return (
@@ -35,31 +43,44 @@ export default function MainNav() {
                     <Navbar.Collapse id="basic-navbar-nav">
 						<Nav className="me-auto">
                             <Link href="/" passHref legacyBehavior><Nav.Link active={router.pathname === "/"} onClick={()=>setIsExpanded(false)}>Home</Nav.Link></Link>
-                            <Link href="/search" passHref legacyBehavior><Nav.Link active={router.pathname === "/search"} onClick={()=>setIsExpanded(false)}>Advanced Search</Nav.Link></Link>
+							{token && 
+								<Link href="/search" passHref legacyBehavior><Nav.Link active={router.pathname === "/search"} onClick={()=>setIsExpanded(false)}>Advanced Search</Nav.Link></Link>
+							}
+							{!token &&
+								<>
+									<Link href="/login" passHref legacyBehavior><Nav.Link active={router.pathname === "/search"} onClick={()=>setIsExpanded(false)}>Login</Nav.Link></Link>
+									<Link href="/register" passHref legacyBehavior><Nav.Link active={router.pathname === "/search"} onClick={()=>setIsExpanded(false)}>Register</Nav.Link></Link>
+								</>
+							}
                         </Nav>
 						&nbsp;
-						<Form className="d-flex" onSubmit={submitForm}>
-							<Form.Control
-							type="search"
-							placeholder="Search"
-							className="me-2"
-							aria-label="Search"
-							value={searchField}
-							onChange={(e)=>setSearchField(e.target.value)}
-							/>
-							<Button variant="outline-success" type="submit">Search</Button>
-						</Form>
+						{token &&
+							<Form className="d-flex" onSubmit={submitForm}>
+								<Form.Control
+								type="search"
+								placeholder="Search"
+								className="me-2"
+								aria-label="Search"
+								value={searchField}
+								onChange={(e)=>setSearchField(e.target.value)}
+								/>
+								<Button variant="outline-success" type="submit">Search</Button>
+							</Form>
+						}
 						&nbsp;
-						<Nav className="d-flex">
-							<NavDropdown title="User Name" id="basic-nav-dropdown">
-								<Link href="/favourites" passHref legacyBehavior>
-									<NavDropdown.Item active={router.pathname === "/favourites"} onClick={()=>setIsExpanded(false)}>Favourites</NavDropdown.Item>
-								</Link>
-								<Link href="/history" passHref legacyBehavior>
-									<NavDropdown.Item active={router.pathname === "/history"} onClick={()=>setIsExpanded(false)}>History</NavDropdown.Item>
-								</Link>
-							</NavDropdown>
-          				</Nav>
+						{token &&
+							<Nav className="d-flex">
+								<NavDropdown title={token.userName} id="basic-nav-dropdown">
+									<Link href="/favourites" passHref legacyBehavior>
+										<NavDropdown.Item active={router.pathname === "/favourites"} onClick={()=>setIsExpanded(false)}>Favourites</NavDropdown.Item>
+									</Link>
+									<Link href="/history" passHref legacyBehavior>
+										<NavDropdown.Item active={router.pathname === "/history"} onClick={()=>setIsExpanded(false)}>History</NavDropdown.Item>
+									</Link>
+									<NavDropdown.Item onClick={()=>logout()}>Logout</NavDropdown.Item>
+								</NavDropdown>
+							</Nav>						
+						}
 						&nbsp;
                     </Navbar.Collapse>
                 </Container>
